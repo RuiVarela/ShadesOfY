e32622e185c9fc1643ca055c9635219ea1878dca
Rui Varela <ryven.kicks.ass@gmail.com>
2022-04-08 18:24:23 +0100

    base effecgt

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-"# ShadesOfY" 
+# ShadesOfY
+
+## Setup
+```
+git clone git@github.com:RuiVarela/ShadesOfY.git
+cd ShadesOfY 
+npm install
+```
+
+## Run
+```
+npm run start
+```
+
+
---
+++ src/fx_base/BaseEffect.js
@@ -0,0 +1,62 @@
+import * as twgl from 'twgl.js';
+
+import vert_source from './base.vert'
+import frag_source from './base.frag'
+
+class BaseEffect {
+    constructor() {
+        this.gl = null;
+        this.arrays = null;
+        this.programInfo = null;
+        this.bufferInfo = null;
+    }
+
+    render(time) {
+        twgl.resizeCanvasToDisplaySize(this.gl.canvas);
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+    
+        const uniforms = this.computeUniforms(time);
+    
+        this.gl.useProgram(this.programInfo.program);
+        twgl.setBuffersAndAttributes(this.gl, this.programInfo, this.bufferInfo);
+        twgl.setUniforms(this.programInfo, uniforms);
+        twgl.drawBufferInfo(this.gl, this.bufferInfo);
+    }
+
+    setup(gl, vert, frag) {
+        this.shutdown();
+
+        if (!vert)
+            vert = vert_source;
+
+        if (!frag)
+            frag = frag_source;
+
+        this.gl = gl;
+        this.arrays = {
+            position: [
+                -1, -1, 0,
+                1, -1, 0,
+                -1, 1, 0,
+                -1, 1, 0,
+                1, -1, 0,
+                1, 1, 0],
+        };
+
+        this.programInfo = twgl.createProgramInfo(this.gl, [vert,frag]);
+        this.bufferInfo = twgl.createBufferInfoFromArrays(this.gl, this.arrays);
+    }
+
+    shutdown() {
+        //TODO:
+    }
+
+    computeUniforms(time) {
+        const uniforms = {
+            time: time * 0.001,
+            resolution: [this.gl.canvas.width, this.gl.canvas.height],
+        };
+        return uniforms;       
+    }
+}
+export default BaseEffect;
