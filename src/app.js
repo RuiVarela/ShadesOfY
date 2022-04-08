bacd7fa7958a907d90dabb705292f5b843667d03
Rui Varela <rui.varela@eyenov.com>
2022-04-08 15:59:11 +0100

    base code

--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
-console.log('Hello world!');
+import * as twgl from 'twgl.js';
+
+import vert_source from './fx_base/base.vert'
+import frag_source from './fx_base/base.frag'
+
+const gl = document.querySelector("#gl_canvas").getContext("webgl");
+const programInfo = twgl.createProgramInfo(gl, [
+    vert_source,
+    frag_source
+]);
+
+const arrays = {
+    position: [
+        -1, -1, 0,
+        1, -1, 0,
+        -1, 1, 0,
+        -1, 1, 0,
+        1, -1, 0,
+        1, 1, 0],
+};
+const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+
+function render(time) {
+    twgl.resizeCanvasToDisplaySize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    const uniforms = {
+        time: time * 0.001,
+        resolution: [gl.canvas.width, gl.canvas.height],
+    };
+
+    gl.useProgram(programInfo.program);
+    twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+    twgl.setUniforms(programInfo, uniforms);
+    twgl.drawBufferInfo(gl, bufferInfo);
+    requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
